a9ee000c5f5e6713f2a4afd4499f11fef11aebed
India Cornell <indiaccornell@gmail.com>
2023-09-03 22:53:45 -0700

    Add Mocha/Chai dependencies and test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
-const request = require('request');
+const request = require("request");
 //Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed not found');
+
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null); //if error, return error value null and the callback function in the index
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body); //parse the body of the response
+      if (data.length === 0) {//if the length of the data is 0, return the callback function with the error message and null
+        callback("Breed not found", null);
+      } else {
+        callback(null, data[0].description);//if the length of the data is not 0, return the callback function with the description of the breed
+      }
     }
-  }
-});
+  });
+};
 
-
-
-//module.exports = { fetchBreedDescription };
+module.exports = { fetchBreedDescription };
